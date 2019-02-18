9b665b1c66c656556785298f99eecef8d454e882
patrik9110 <30991803+patrik9110@users.noreply.github.com>
2019-02-17 21:58:08 -0300

    finished the youtube tutorial

--- Beautiful Soup Tutorial - Web Scraping in Python/bs_objects.py
+++ Beautiful Soup Tutorial - Web Scraping in Python/bs_objects.py
@@ -22,5 +22,37 @@ and they lived at the bottom of a well.</p>
 #    f.write(html_doc)
 
 soup = BeautifulSoup(html_doc, "html.parser")
-print(soup.prettify())
-#                                               ME QUEDE EN EL MIN 21:40
+#print(soup.prettify())
+#print(soup.title)
+#print(soup.find("title"))
+#print(soup.find_all("b"))
+
+#tag = soup.b
+#tag.name = "blockquote"
+#print(tag)
+
+#tag = soup.find_all("b")[2]
+#print(tag)
+#print(tag["id"])
+
+#tag = soup.find_all("b")[3]
+#print(tag.attrs)
+
+#tag = soup.find_all("b")[3]
+#print(tag)
+#tag["another-attribute"] = 2
+#print(tag)
+
+#tag = soup.find_all("b")[3]
+#print(tag)                      the complete tag
+#del tag["id"]                   erase the "id" atribute
+#print(tag)                      the tag without the "id" atribute
+#del tag["another-attribute"]    erase the "another-atribute" atribute
+#print(tag)                      the tag without any atributes
+
+#tag = soup.find_all("b")[3]
+#print(tag)
+#print(tag.string)
+#tag.string.replace_with("This is another string")
+#print(tag)
+#print(tag.string)
